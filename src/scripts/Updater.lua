37c80546e297f1deb5d9118a5bb4fdb715204077
gesslar <karahd@gmail.com>
2024-06-23 21:35:30 -0400

    eek i had multiple copies of the script

--- src/scripts/Updater.lua
+++ src/scripts/Updater.lua
@@ -4,39 +4,6 @@ __PKGNAME__.MupdateUser = "__PKGNAME__.AutoMupdate"
 __PKGNAME__.MupdateFunction = "__PKGNAME__:AutoMupdate"
 
 function __PKGNAME__:AutoMupdate(handle)
-    debugc("AutoMupdate - Package Name: __PKGNAME__, Handle: " .. handle)
-    if handle ~= self.MupdateUser then return end
-
-    local timerName = "__PKGNAME__.AutoMupdate"
-    local timers = getNamedTimers(self.MupdateUser)
-
-    for _, timer in ipairs(timers) do
-        if timer.name == timerName then return end
-    end
-
-    registerNamedTimer(self.MupdateUser, self.MupdateUser, 2, function()
-        deleteAllNamedTimers(self.MupdateUser)
-
-        local Mupdate = require("__PKGNAME__\\Mupdate")
-        local updater = Mupdate:new({
-            download_path = "https://github.com/gesslar/__PKGNAME__/releases/latest/download/",
-            package_name = "__PKGNAME__",
-            remote_version_file = "version.txt",
-            debug_mode = true,
-        })
-        updater:Start(function()
-            updater:Cleanup()
-            updater = nil
-        end)
-    end)
-end
-
--- Auto Updater
-__PKGNAME__ = __PKGNAME__ or {}
-__PKGNAME__.MupdateUser = "__PKGNAME__.AutoMupdate"
-__PKGNAME__.MupdateFunction = "__PKGNAME__:AutoMupdate"
-
-function __PKGNAME__:AutoMupdate(handle)
   debugc("AutoMupdate - Package Name: __PKGNAME__, Handle: " .. handle)
   if handle ~= self.MupdateUser then return end
 
